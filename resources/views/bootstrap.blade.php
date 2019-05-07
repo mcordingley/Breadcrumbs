33435ace1a8f2f923676151d0506d9ffd7a909f5
Michael Cordingley <Michael.Cordingley@gmail.com>
2019-05-07 15:01:06 -0400

    Add view partial for Bootstrap.

---
+++ resources/views/bootstrap.blade.php
@@ -0,0 +1,11 @@
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        @foreach ($trail as $crumb)
+            @if ($loop->last)
+                <li class="breadcrumb-item active" aria-current="page">{{ $crumb->title() }}</li>
+            @else
+                <li class="breadcrumb-item"><a href="{{ $crumb->url() }}">{{ $crumb->title() }}</a></li>
+            @endif
+        @endforeach
+    </ol>
+</nav>
