8ceb7003a0ab23426c8f61521b269dae900fa9d0
Michael Cordingley <Michael.Cordingley@gmail.com>
2019-05-08 00:57:07 -0400

    An empty trail should mean no markup in the Bootstrap view.

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -1,11 +1,13 @@
-<nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        @foreach ($trail as $crumb)
-            @if ($loop->last)
-                <li class="breadcrumb-item active" aria-current="page">{{ $crumb->title() }}</li>
-            @else
-                <li class="breadcrumb-item"><a href="{{ $crumb->url() }}">{{ $crumb->title() }}</a></li>
-            @endif
-        @endforeach
-    </ol>
-</nav>
+@if (count($trail))
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            @foreach ($trail as $crumb)
+                @if ($loop->last)
+                    <li class="breadcrumb-item active" aria-current="page">{{ $crumb->title() }}</li>
+                @else
+                    <li class="breadcrumb-item"><a href="{{ $crumb->url() }}">{{ $crumb->title() }}</a></li>
+                @endif
+            @endforeach
+        </ol>
+    </nav>
+@endif
