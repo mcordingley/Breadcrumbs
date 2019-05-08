77097f4a973f68c773531e025c9962bcc7a77616
Michael Cordingley <Michael.Cordingley@gmail.com>
2019-05-08 00:49:16 -0400

    Rename facade.

---
+++ src/Breadcrumb.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MCordingley\Breadcrumbs;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static Trail make(string $path, array $properties = [])
+ * @see Factory
+ */
+final class Breadcrumb extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'breadcrumbs';
+    }
+}
